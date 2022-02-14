da01c01ce2ebe9370d7d0fa51c72a90a11f51f72
admin <admin@teju2629>
2022-02-14 09:00:34 +0530

    leap buffered

---
+++ Leap/src/Leapyear.java
@@ -0,0 +1,26 @@
+
+
+import java.io.*;
+import java.io.*;
+public class Leapyear
+{
+ public static void main(String args[]) throws IOException
+ {
+  String n;
+   
+  System.out.println("\n\nEnter the Year:\t");                
+  BufferedReader ob=new BufferedReader(new InputStreamReader(System.in));
+
+  n = ob.readLine();
+  int leap_year = Integer.parseInt(n);
+               
+  if(leap_year % 4 == 0)
+  {
+       System.out.println("\n\tThis Year is Leap");
+  }
+  else
+  {
+       System.out.println("\n\tThis Year is not Leap");
+  }
+ }
+}
