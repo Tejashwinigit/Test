44aca1129b8af2b7027414ddcd79e964b6d94d4c
admin <admin@teju2629>
2022-02-19 18:58:57 +0530

    first commit

---
+++ final keyword/src/Class.java
@@ -0,0 +1,9 @@
+public class Class {
+  final int Y = 10;
+
+  public static void main(String[] args) {
+    Class myObj = new Class();
+  //  myObj.Y = 25; 
+    System.out.println(myObj.Y);
+  }
+}
