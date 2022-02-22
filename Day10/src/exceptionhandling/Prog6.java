3f753d4099ede3099f5224fdba12805cfe472598
admin <admin@teju2629>
2022-02-22 13:40:45 +0530

    commit

--- Day10/src/exceptionhandling/Prog6.java
+++ Day10/src/exceptionhandling/Prog6.java
@@ -3,18 +3,21 @@ package exceptionhandling;
 
 	import java.io.*;
 
-	class Prog6 {
+	public class Prog6 {
 	  public static void findFile() {
 try {
 	
-}
+
 	       File newFile = new File("test.txt");
 	    FileInputStream stream = new FileInputStream(newFile);
 	  }
 	
-
+catch(ArithmeticException e) 
+{
+	
+}
 	  public static void main(String[] args) {
-	    System.out.println(“Print the exception”);
+	    System.out.println(“hello”);
 	  }
 	}
 
