2d77e93074721a43046bec466195a3f0bcdec07b
admin <admin@teju2629>
2022-02-22 13:40:15 +0530

    commit

---
+++ Day10/src/exceptionhandling/Prog6.java
@@ -0,0 +1,23 @@
+package exceptionhandling;
+
+
+	import java.io.*;
+
+	class Prog6 {
+	  public static void findFile() {
+try {
+	
+}
+	       File newFile = new File("test.txt");
+	    FileInputStream stream = new FileInputStream(newFile);
+	  }
+	
+
+	  public static void main(String[] args) {
+	    System.out.println(“Print the exception”);
+	  }
+	}
+
+
+	
+
