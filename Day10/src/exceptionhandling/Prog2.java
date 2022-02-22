060d61c4e841c0a02a72f6af84c33d0f9d70234e
admin <admin@teju2629>
2022-02-22 11:14:38 +0530

    commit

---
+++ Day10/src/exceptionhandling/Prog2.java
@@ -0,0 +1,27 @@
+package exceptionhandling;
+public class Prog2
+{
+public static void main (String[] args) {
+	
+	
+		
+	
+			
+		try {
+			int a=10;
+	for(int i=3;i>=0;i--)
+	{
+		System.out.println(a/i);
+	}
+	}
+	catch (ArithmeticException e)
+	{
+		System.out.println(e);
+	}
+		
+	}
+		
+	
+		}
+
+	
