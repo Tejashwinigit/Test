9c93aa58383a0fa5ea4cd29bc23e1799e69e7904
admin <admin@teju2629>
2022-02-24 12:20:55 +0530

    commit

---
+++ Day11/src/Prog1.java
@@ -0,0 +1,12 @@
+
+
+
+import java.util.*;
+
+public class Prog1 {
+    public static void main (String[] args) {
+        System.out.println(5/0);
+        System.out.println("End of program!");
+	}
+}
+
