91f958ddd63e3e528aecad3bcf9bc67a9019d26e
admin <admin@teju2629>
2022-02-25 13:40:36 +0530

    first commit

---
+++ Iterator/src/Program.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Program {
+  public static void main(String[] args) {
+
+    // Make a collection
+    ArrayList<String> cars = new ArrayList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+
+    // Get the iterator
+    Iterator<String> it = cars.iterator();
+
+    // Print the first item
+    System.out.println(it.next());
+  }
+}
